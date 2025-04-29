b7c503cb092ed2be9e7a42af45a06768cbc82ecf
SH-ke <shke_dev@163.com>
2025-04-29 20:23:18 +0800

    update mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,20 +1,22 @@
 local plugins = {
-      "williamboman/mason.nvim",
-      opts = {
-        ensure_installed = {
-          "black",
-          "debugpy",
-          "mypy",
-          "ruff",
-          "pylsp",
-          "python-lsp-server",
-          "clangd",
-          "clang-format",
-          "codelldb",
-        },
-        run_on_start = true,
-        log_level = vim.log.levels.DEBUG,
-      },
+  -- Run the MasonInstallAll command
+  "williamboman/mason.nvim",
+  install_root_dir = vim.fn.stdpath("data") .. "/mason",
+  opts = {
+    ensure_installed = {
+      "black",
+      "debugpy",
+      "mypy",
+      "ruff",
+      "pylsp",
+      "python-lsp-server",
+      "clangd",
+      "clang-format",
+      "codelldb",
+    },
+    run_on_start = true,
+    log_level = vim.log.levels.DEBUG,
+  },
 }
 
 return plugins
