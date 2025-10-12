fa15fbd3e281176d5139e8043750f295754ae02e
SH-ke-inspire <shke_dev@163.com>
2025-10-12 15:14:44 +0800

    fix ts compiler

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,30 @@
 -- lua/plugins/treesitter.lua
 local plugins = {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-      --... 其他配置
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-        "html", "css",
-        "bash",
-        "c",
-        "cpp",        -- 添加 C++
-        "go",         -- 添加 Golang
-        "json",
-        "markdown",
-        "python",
-        "rust",
-        "typst",      -- 添加 Typst
-        "latex",      -- 添加 LaTeX
-  		},
-  	},
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua", "python", "c", "cpp",
+  		"vim", "lua", "vimdoc",
+      "html", "css",
+  		"bash",
+  		"go",         -- 添加 Golang
+  		"json",
+  		"markdown",
+  		"rust",
+  		"typst",      -- 添加 Typst
+  		"latex",      -- 添加 LaTeX
+    },
+  },
+
+  config = function(_, opts)
+    -- 🌱 先设置安装行为
+    local install = require("nvim-treesitter.install")
+    install.prefer_git = false
+    install.compilers = { "zig", "clang" }
+
+    -- 🌳 再加载 treesitter 主配置
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
 
 return plugins
